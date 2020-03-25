890feb55fb212d2ea18f4db03baee7449881889e
Davin Jaya <davinjayawork@gmail.com>
2020-03-25 22:32:26 +0800

    Added function to dynamically render the page setting and add multifield modal

--- src/components/page-setting/page-setting-user-edit/pageSettingUserEdit.js
+++ src/components/page-setting/page-setting-user-edit/pageSettingUserEdit.js
@@ -1,13 +1,19 @@
 import React, { Fragment, Component } from 'react';
 
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 import { withRouter } from "react-router-dom";
 
 class PageSettingUserEdit extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            modal: false,
             data: [],
             //Form
+            addFormName:'customDataValueAddEditForm',
+            activeCustomData: null,
+            newDataModal: true,
             //Form
             pageKey: ''
         }
@@ -172,7 +178,7 @@ class PageSettingUserEdit extends Component {
                                     {
                                         "cdsId": 3,
                                         "cdsName": "Trending menu",
-                                        "cdsKey": null,
+                                        "cdsKey": "trending.menu",
                                         "cdsType": 2,
                                         "cdsSequence": "3",
                                         "cdId": null
@@ -180,7 +186,7 @@ class PageSettingUserEdit extends Component {
                                     {
                                         "cdsId": 2,
                                         "cdsName": "Menu Name",
-                                        "cdsKey": null,
+                                        "cdsKey": "menu.name",
                                         "cdsType": 1,
                                         "cdsSequence": "2",
                                         "cdId": null
@@ -188,7 +194,7 @@ class PageSettingUserEdit extends Component {
                                     {
                                         "cdsId": 1,
                                         "cdsName": "Menu Picture",
-                                        "cdsKey": null,
+                                        "cdsKey": "menu.picture",
                                         "cdsType": 1,
                                         "cdsSequence": "1",
                                         "cdId": null
@@ -213,18 +219,25 @@ class PageSettingUserEdit extends Component {
 
     }
 
-    renderInputFieldBasedOnType(fieldSetting, value) {
-        switch(fieldSetting.cdsType) {
+    renderInputField(setting, value) {
+        switch(setting.cdsType) {
             //Text field
             case 1:
                 return (
                     <div className="form-group">
-                        Type 1
-                        <label className="col-form-label" htmlFor={fieldSetting.cdsKey}>{fieldSetting.cdsName}</label>
-                        <input className="form-control" type="text" id={fieldSetting.cdsKey} name={fieldSetting.cdsKey} />
+                        <label className="col-form-label" htmlFor={setting.cdsKey}>{setting.cdsName}</label>
+                        <input className="form-control" type="text" id={setting.cdsKey} name={setting.cdsKey} value={value ? value : ""}/>
                     </div>
                 );
-            //Boolean
+            //Checkboxes
+            case 2:
+                return (
+                    <div className="custom-control custom-checkbox mb-3">
+                      <input className="custom-control-input" id={setting.cdsKey} type="checkbox" required />
+                      <label className="custom-control-label" htmlFor={setting.cdsKey}>{setting.cdsName}</label>
+                      <div className="invalid-feedback">Example invalid feedback text</div>
+                    </div>
+                );
             //Blob
             //Textarea
             default:
@@ -234,17 +247,111 @@ class PageSettingUserEdit extends Component {
         }
     }
 
-    renderInputSectionBasedOnType(customData) {
+    //Set Active Custom Data and toggle modal
+    setActiveCustomData(customData, newData) {
+        this.setState({
+            activeCustomData: customData,
+            newDataModal: newData
+        }, () => {
+            this.toggleModal();
+            console.log(this.state.activeCustomData, this.state.newDataModal);
+
+        });
+    }
+
+    renderMultiTable = (fieldSettings, fieldValue, customData) => {
+        //To sort by the sequence of the custom data settings
+        let sortFieldSettings = (a,b) => {
+            if(Number.parseInt(a["cdsSequence"]) > Number.parseInt(b["cdsSequence"])) return 1;
+            if(Number.parseInt(b["cdsSequence"]) > Number.parseInt(a["cdsSequence"])) return -1;
+
+            return 0;
+        }
+
+        fieldSettings.sort(sortFieldSettings);
+        //To sort by the sequence of the custom data settings
+        let renderTableValue = (values) => {
+            return(
+                <Fragment>
+                    {values.map(value => {
+                        return (
+                            <Fragment>
+                                {fieldSettings.map(setting => {
+                                    return(
+                                        <Fragment>
+                                            {(value[setting["cdsKey"]]) ? 
+                                                <td>{value[setting["cdsKey"]].value}</td>
+                                                :
+                                                <td></td>
+                                            }
+                                        </Fragment>
+                                    );
+                                })}
+                            </Fragment>
+                        )
+                    })}
+                    <td>
+                        <span>
+                            <i className="fa fa-trash" style={{ width: 35, fontSize: 16, padding: 11, color: '#e4566e' }}
+                            ></i>
+                        </span>
+                        <span>
+                            <i className="fa fa-pencil" onClick={() => {this.setActiveCustomData(customData, false);}} style={{ width: 35, fontSize: 16, padding: 11, color: 'rgb(40, 167, 69)' }}></i>
+                        </span>
+                    </td>
+                </Fragment>
+            );
+        }
+        return (
+            <div className="table-responsive">
+                <table className="table table-bordered">
+                    <thead className="thead-dark">
+                        <tr>
+                            {fieldSettings.map(setting => {
+                                return(
+                                    <th scope="col">{setting.cdsName}</th>
+                                );
+                            })}
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {(fieldValue && fieldValue.length > 0) ? 
+                                renderTableValue(fieldValue)
+                                : 
+                                <td className="text-center" colSpan={fieldSettings.length + 1}>You haven't add any value yet</td>
+                            }
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    renderSectionBasedOnType(customData) {
         if (customData.cdSettingList && customData.cdSettingList.length > 0) {
+            let fieldSettings,
+            fieldValue;
             switch (customData.cdType) {
                 //Single Data
                 case 1:
-                    let fieldSetting = customData.cdSettingList[0],
-                        fieldValue = "";
+                    fieldSettings = customData.cdSettingList[0];
                     if(customData.cdValuePair && customData.cdValuePair.length > 0) {
                         fieldValue = customData.cdValuePair[0];
                     }
-                    return this.renderInputFieldBasedOnType(fieldSetting, fieldValue);
+                    return (
+                        <Fragment>
+                            {this.renderInputField(fieldSettings, fieldValue.value)}
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="d-flex align-items-center justify-content-end">
+                                        <button className="btn btn-primary mr-1">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Fragment>
+                        );
                 //2 - Array
                 case 2:
                     return (
@@ -254,26 +361,75 @@ class PageSettingUserEdit extends Component {
                     );
                 //3 - Multifield
                 case 3:
+                    fieldSettings = customData.cdSettingList;
+                    if(customData.cdValuePair && customData.cdValuePair.length > 0) {
+                        fieldValue = customData.cdValuePair;
+                    }
+
                     return (
-                        <div className="form-group">
-                            Type 3
-                        </div>
+                        <Fragment>
+                            <div className="form-group">
+                                <div className="row">
+                                    <div className="col-12">
+                                        {this.renderMultiTable(fieldSettings, fieldValue, customData)}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="d-flex align-items-center justify-content-end">
+                                        <button type="button" className="btn btn-primary mr-1" onClick={() => {this.setActiveCustomData(customData, true);}}>Add New</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Fragment>
                     );
                 default:
                     return (
-                        <span>Error when rendering custom data section</span>
+                        <span>Error when rendering custom data section<br></br></span>
                     );
             }
         }
         else {
             return (
-                <span>Custom Data Setting is not yet set for custom data with key "{customData.cdKey}"</span>
+                <span>Custom Data Setting is not yet set for custom data with key "{customData.cdKey}"<br></br></span>
             );
         }
     }
 
+    toggleModal = () => {
+        const { modal } = this.state;
+        this.setState({
+            modal: !modal
+        });
+    }
+
+    constructModalBody = () => {
+        const { addFormName, activeCustomData, newDataModal } = this.state;
+
+        return (
+            <Fragment>
+                <ModalBody>
+                    <form id={addFormName} name={addFormName}>
+                        {newDataModal ? 
+                            activeCustomData.cdSettingList.map(setting => {
+                                return this.renderInputField(setting);
+                            }) 
+                            : 
+                            ""
+                        }
+                    </form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" form={addFormName} type="submit">Save Changes</Button>
+                    <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                </ModalFooter>
+            </Fragment>
+        );
+    }
+
     render() {
-        const { data } = this.state;
+        const { modal, data, activeCustomData, newDataModal } = this.state;
 
         if (!data || data.length <= 0) {
             return (
@@ -287,8 +443,21 @@ class PageSettingUserEdit extends Component {
                 <Fragment>
                     {data.map((customGroup, index) => {
                         let formName = `form${index}`;
+                        //To sort by the sequence of the custom data
+                        let sortCustomData = (a,b) => {
+                            if(Number.parseInt(a["cdSequence"]) > Number.parseInt(b["cdSequence"])) return 1;
+                            if(Number.parseInt(b["cdSequence"]) > Number.parseInt(a["cdSequence"])) return -1;
+                
+                            return 0;
+                        }
+                
+                        if (customGroup.customDataList && customGroup.customDataList.length > 0) {
+                            customGroup.customDataList.sort(sortCustomData);
+                        }
+                        //To sort by the sequence of the custom data
+            
                         return (
-                            <div className="col-12 col-lg-6">
+                            <div className="col-12">
                                 <div className="card">
                                     <div className="card-header">
                                         <h5>{customGroup.cdGroupName}</h5>
@@ -297,11 +466,18 @@ class PageSettingUserEdit extends Component {
                                     <div className="card-body datatable-react">
                                         <div className="row">
                                             <div className="col-12">
-                                                <form id={formName} name={formName}>
+                                                <form id={formName} name={formName} className="theme-form">
                                                     {(customGroup.customDataList && customGroup.customDataList.length > 0) ?
                                                         <Fragment>
-                                                            {customGroup.customDataList.map(customData => {
-                                                                return this.renderInputSectionBasedOnType(customData);
+                                                            {customGroup.customDataList.map((customData, index) => {
+                                                                // return this.renderInputSectionBasedOnType(customData);
+                                                                return (
+                                                                    <Fragment>
+                                                                        {(index != 0) ? <hr className="mt-4 mb-4"></hr> : ""}
+                                                                        <h6>{customData.cdName}</h6>
+                                                                        {this.renderSectionBasedOnType(customData)}
+                                                                    </Fragment>
+                                                                );
                                                             })}
                                                         </Fragment>
                                                         :
@@ -315,6 +491,10 @@ class PageSettingUserEdit extends Component {
                             </div>
                         );
                     })}
+                    <Modal isOpen={modal} toggle={this.toggleModal} size="lg">
+                        <ModalHeader toggle={this.toggleModal}>{newDataModal ? "New Value" : "Update Value"}</ModalHeader>
+                        {activeCustomData ? this.constructModalBody() : <Fragment> Loading Data... </Fragment>}
+                    </Modal>
                 </Fragment>
             );
         }
