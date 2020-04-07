fc8304d1cb7ce78cd3746f0067a5b34a2351ad05
Davin Jaya <davinjayawork@gmail.com>
2020-04-07 17:20:30 +0800

    Fix issue 1

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Breadcrumb from '../components/common/breadcrumb';
-import { DollarSign, MapPin, X, TrendingDown, ArrowUp, ShoppingCart, Search, Activity, User } from 'react-feather';
-import Slider from 'react-slick';
+import { FileText } from 'react-feather';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import PageSettingList from '../components/page-setting/page-setting-list/pageSettingList';
@@ -10,44 +9,30 @@ import CountUp from 'react-countup';
 const Dashboard = () => {
 
 
-  const [settings, setSettings] = useState({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    // variableWidth:true,
-    arrows: false,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 370,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
-  });
+  const [totalQuotation, setTotalQuotation] = useState(0);
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetch("/segosarem-backend/getQuotationList", {
+        method: 'POST',
+        body: JSON.stringify({}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "token": localStorage.getItem("AdminToken")
+        }
+    }).then(res => res.json())
+        .then((result) => {
+            switch(result.returnCode) {
+                case "000000":
+                    setTotalQuotation(result.responseObject.length);
+                    break;
+                case "333333":
+                    this.props.history.push(`${process.env.PUBLIC_URL}/login`);
+                    break;
+                default:
+                    break;
+            }
+        });
+  }, []);
 
   return (
     <Fragment>
@@ -56,96 +41,30 @@ const Dashboard = () => {
         <div className="row">
           <div className="col-xl-7 xl-100">
             <div className="row">
-              <div className="col-md-12 ecommerce-slider" >
-                <Slider {...settings}>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <DollarSign />
-                        <div><span>Total Earning</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={72} /></h4>
-                      </div>
+              <div className="col-xs-6 col-md-4" >
+                <div className="item">
+                  <div className="card">
+                    <div className="card-body ecommerce-icons text-center">
+                      <FileText />
+                      <div><span>Total Quotation</span></div>
+                      <h4 className="font-primary mb-0">
+                        <CountUp className="counter" end={totalQuotation} /></h4>
                     </div>
                   </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <MapPin />
-                        <div><span>Total Web Visitor</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={65} /></h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <ShoppingCart />
-                        <div><span>Total Sale Product</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={96} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <TrendingDown />
-                        <div><span>Company Loss</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={89} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <DollarSign />
-                        <div><span>Total Earning</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={72} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <MapPin />
-                        <div><span>Total Web Visitor</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={65} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <ShoppingCart />
-                        <div><span>Total Sale Product</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={96} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="item">
-                    <div className="card">
-                      <div className="card-body ecommerce-icons text-center">
-                        <TrendingDown />
-                        <div><span>Company Loss</span></div>
-                        <h4 className="font-primary mb-0">
-                          <CountUp className="counter" end={89} />
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                </Slider>
+                </div>
               </div>
+              {/* <div className="col-xs-6 col-md-4" >
+                <div className="item">
+                  <div className="card">
+                    <div className="card-body ecommerce-icons text-center">
+                      <MapPin />
+                      <div><span>Total Web Visitor</span></div>
+                      <h4 className="font-primary mb-0">
+                        <CountUp className="counter" end={65} /></h4>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
             </div>
           </div>
           <div className="col-xl-12">
