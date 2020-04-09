97cae4950dd7f6838d42b704f0565f8ee0983431
Dimas Natanael <dimznatanael42@gmail.com>
2020-04-10 00:39:39 +0700

    Page List Card View

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,6 +4,7 @@ import { FileText } from 'react-feather';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import PageSettingList from '../components/page-setting/page-setting-list/pageSettingList';
+import PageSettingCards from '../components/page-setting/page-setting-cards/pageSettingCards';
 import CountUp from 'react-countup';
 
 const Dashboard = () => {
@@ -13,30 +14,30 @@ const Dashboard = () => {
 
   useEffect(() => {
     fetch("/segosarem-backend/getQuotationList", {
-        method: 'POST',
-        body: JSON.stringify({}),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "token": localStorage.getItem("AdminToken")
-        }
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "token": localStorage.getItem("AdminToken")
+      }
     }).then(res => res.json())
-        .then((result) => {
-            switch(result.returnCode) {
-                case "000000":
-                    setTotalQuotation(result.responseObject.length);
-                    break;
-                case "333333":
-                    this.props.history.push(`${process.env.PUBLIC_URL}/login`);
-                    break;
-                default:
-                    break;
-            }
-        });
+      .then((result) => {
+        switch (result.returnCode) {
+          case "000000":
+            setTotalQuotation(result.responseObject.length);
+            break;
+          case "333333":
+            this.props.history.push(`${process.env.PUBLIC_URL}/login`);
+            break;
+          default:
+            break;
+        }
+      });
   }, []);
 
   return (
     <Fragment>
-      <Breadcrumb parent="Dashboard" title="Ecommerce" />
+      <Breadcrumb parent="Dashboard" title="Statistics" />
       <div className="container-fluid">
         <div className="row">
           <div className="col-xl-7 xl-100">
@@ -67,19 +68,13 @@ const Dashboard = () => {
               </div> */}
             </div>
           </div>
+        </div>
+      </div>
+      <Breadcrumb parent="Dashboard" title="Page List" />
+      <div className="container-fluid">
+        <div className="row">
           <div className="col-xl-12">
-            <div className="card">
-              <div className="card-header">
-                <h5>Page List</h5>
-              </div>
-              <div className="card-body datatable-react">
-                <div className="row mt-4">
-                  <div className="col-12">
-                    <PageSettingList excludeDelete={true} excludeEdit={true} redirectUrl={`${process.env.PUBLIC_URL}/pageSetting`}/>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PageSettingCards redirectUrl={`${process.env.PUBLIC_URL}/pageSetting`} />
           </div>
         </div>
       </div>
