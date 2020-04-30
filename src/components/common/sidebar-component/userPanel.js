830e6e579cea910371071073220cf6bfe9be0e94
Dimas Natanael <dimznatanael42@gmail.com>
2020-05-01 02:14:21 +0700

    Changed Admin Name and Picture

--- src/components/common/sidebar-component/userPanel.js
+++ src/components/common/sidebar-component/userPanel.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
-import man from '../../../assets/images/dashboard/user.png'
+// import man from '../../../assets/images/dashboard/user.png'
+import logo from '../../../assets/images/logo-sego-sarem.png'
 import { Link } from 'react-router-dom';
 import { Edit } from 'react-feather';
 
@@ -27,15 +28,15 @@ const UserPanel = props => {
         <Fragment>
             <div className="sidebar-user text-center">
                 <div>
-                    <img className="img-60 rounded-circle lazyloaded blur-up" src={url ? url : man} alt="#" />
+                    <img className="img-60 rounded-circle lazyloaded blur-up" src={logo} alt="#" />
                     <div className="profile-edit">
                         <Link to={`${process.env.PUBLIC_URL}/users/userEdit`}>
                             <Edit />
                         </Link>
                     </div>
                 </div>
-                <h6 className="mt-3 f-14">ELANA</h6>
-                <p>general manager.</p>
+                <h6 className="mt-3 f-14">SEGO SAREM CAK BOYO</h6>
+                <p>Admin</p>
             </div>
         </Fragment>
     );
